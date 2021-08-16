b846076de468bd5039d90cd7fe4605983c8d5a78
shion-4711 <77258886+shion-4711@users.noreply.github.com>
2021-08-16 17:54:42 +0900

    create each functions

---
+++ src/components/organisms/user/UserDetailModal.tsx
@@ -0,0 +1,101 @@
+import { ChangeEvent, memo, useEffect, useState, VFC } from "react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  Stack,
+  FormControl,
+  Input,
+  FormLabel,
+  ModalFooter
+} from "@chakra-ui/react";
+
+import { User } from "../../../types/api/user";
+import { PrimaryButton } from "../../atoms/button/PrimaryButton";
+
+type Props = {
+  user: User | null;
+  isOpen: boolean;
+  isAdmin?: boolean;
+  onClose: () => void;
+};
+
+export const UserDetailModal: VFC<Props> = memo((props) => {
+  const { user, isOpen, isAdmin = false, onClose } = props;
+  const [username, setUserName] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  useEffect(() => {
+    setUserName(user?.username ?? "");
+    setName(user?.name ?? "");
+    setEmail(user?.email ?? "");
+    setPhone(user?.phone ?? "");
+  }, [user]);
+
+  const onChangeUserName = (e: ChangeEvent<HTMLInputElement>) =>
+    setUserName(e.target.value);
+  const onChangeName = (e: ChangeEvent<HTMLInputElement>) =>
+    setUserName(e.target.value);
+  const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) =>
+    setUserName(e.target.value);
+  const onChangePhone = (e: ChangeEvent<HTMLInputElement>) =>
+    setUserName(e.target.value);
+
+  const onClick = () => alert("更新");
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} autoFocus={false}>
+      <ModalOverlay />
+      <ModalContent pb={6}>
+        <ModalHeader>ユーザー詳細</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody mx={4}>
+          <Stack spacing={4}>
+            <FormControl>
+              <FormLabel>名前</FormLabel>
+              <Input
+                value={username}
+                onChange={onChangeUserName}
+                isReadOnly={!isAdmin}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>フルネーム</FormLabel>
+              <Input
+                value={name}
+                onChange={onChangeName}
+                isReadOnly={!isAdmin}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>メール</FormLabel>
+              <Input
+                value={email}
+                onChange={onChangeEmail}
+                isReadOnly={!isAdmin}
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>電話番号</FormLabel>
+              <Input
+                value={phone}
+                onChange={onChangePhone}
+                isReadOnly={!isAdmin}
+              />
+            </FormControl>
+          </Stack>
+        </ModalBody>
+        {isAdmin && (
+          <ModalFooter>
+            <PrimaryButton onClick={onClick}>更新</PrimaryButton>
+          </ModalFooter>
+        )}
+      </ModalContent>
+    </Modal>
+  );
+});
